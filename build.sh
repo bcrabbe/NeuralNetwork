f386ca820fb9980231a420212d739cb2a88ccd64
bcrabbe <bencrabbe@hotmail.co.uk>
2015-08-06 10:16:32 +0100

    final

---
+++ build.sh
@@ -0,0 +1,5 @@
+export CLASSPATH=./jblas/target/jblas-1.2.4-SNAPSHOT.jar:.
+
+javac  Driver.java HiddenLayer.java Network.java Neuron.java Trainer.java OutputLayer.java DataList.java DataPoint.java GraphWindow.java Grapher.java 
+java Driver 
+
